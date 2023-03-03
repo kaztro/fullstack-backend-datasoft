e720c9bbe1113506131ec88172b0d8881ad871a3
kaztro <00229017@uca.edu.sv>
2023-03-03 17:14:01 -0600

    Project init

---
+++ src/main/java/com/kaztro/fullstackbackenddatasoft/FullstackBackendDatasoftApplication.java
@@ -0,0 +1,13 @@
+package com.kaztro.fullstackbackenddatasoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackBackendDatasoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackBackendDatasoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaztro/fullstackbackenddatasoft/FullstackBackendDatasoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaztro.fullstackbackenddatasoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackBackendDatasoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
