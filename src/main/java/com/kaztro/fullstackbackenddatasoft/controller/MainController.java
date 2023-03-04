d90a2ce75f5872c42a63b6040450f3477480ad9c
kaztro <00229017@uca.edu.sv>
2023-03-03 23:42:06 -0600

    Se configuró el crossorigin

--- src/main/java/com/kaztro/fullstackbackenddatasoft/controller/MainController.java
+++ src/main/java/com/kaztro/fullstackbackenddatasoft/controller/MainController.java
@@ -3,6 +3,7 @@ package com.kaztro.fullstackbackenddatasoft.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import com.kaztro.fullstackbackenddatasoft.model.PostRequest;
 import com.kaztro.fullstackbackenddatasoft.model.Product;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class MainController {
 
     @Autowired
